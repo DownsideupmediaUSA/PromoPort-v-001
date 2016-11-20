ad8976a4956fa0b1dbe44658d3f5e7bb430fc3fb
DownsideupmediaUSA <brandondecarlo@live.com>
2016-11-19 20:58:30 -0500

    First push... stubbed out file structure, migrations, devise, ommiauth-fb

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root "home#index"
+
+
+  match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
+  match 'auth/failure', to: redirect('/'), via: [:get, :post]
+  match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
+end
