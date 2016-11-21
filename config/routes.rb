55462d2e811aa9193d49f5ed76602bb974ff0cd5
DownsideupmediaUSA <brandondecarlo@live.com>
2016-11-21 10:26:56 -0500

    refactored config/routes and cooresponding links back to :delete... sign out fixed properly now

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
-
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
 
 
   root "users#show"
@@ -20,7 +22,7 @@ Rails.application.routes.draw do
 
   match 'auth/:facebook/callback', to: 'sessions#create', via: [:get, :post]
   match 'auth/failure', to: redirect('/'), via: [:get, :post]
-  match 'users/sign_out' => "devise/sessions#destroy", via: [:get, :post]
+  # match 'users/sign_out' => "devise/sessions#destroy", via: [:get, :post]
 
 
 end
