7b666b7812e659e83a6b357e429ae484d152aa0d
DownsideupmediaUSA <brandondecarlo@live.com>
2016-11-26 20:05:47 -0500

    worked on (ongoing) artist link on song show page, added drop down for user(name)for comments form

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,10 @@
 class CommentsController < ApplicationController
 
   def create
-
-    @song = Song.find_by(params[:song_id])
+    @song = Song.find(params[:song_id])
     @comment = Comment.new(comment_params)
     @comment.save
-    redirect_to comment.song
+    redirect_to song_path(@song)
   end
 
   def show
@@ -14,13 +13,12 @@ class CommentsController < ApplicationController
 
   def index
     @comments = Comment.all
-
   end
 
   private
 
   def comment_params
-    params.require(:comment).permit(:content, :song_id, :name)
+    params.require(:comment).permit(:content, :song_id, :user_id, user_attributes:[:name])
   end
 
 
