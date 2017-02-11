49a0bad4a77eb822c5cb3e5733cd2fc584ad187a
DownsideupmediaUSA <brandondecarlo@live.com>
2017-02-10 21:51:43 -0500

    completed js-ajax show functionality

--- app/assets/javascripts/songs/songs.js
+++ app/assets/javascripts/songs/songs.js
@@ -21,13 +21,13 @@ $(function() {
          $(document).on('click', '.song-title', function(e) {
            e.preventDefault()
            const id = $(this).data('id')
-           fetch(`/api/songs/${id}`)
-             .then(response => response.json())
-             .then(song => {
-             $('#trackBanner').html('')
-             const newSong = new Song(song.id, song.title, song.artist, song.comments)
-             const formattedSong = newSong.formatSongShow()
-             $('#trackBanner').append(formattedSong)
+            fetch(`/api/songs/${id}`)
+              .then(response => response.json())
+              .then(song => {
+                $('.trackBanner').html('')
+                const newSong = new Song(song.id, song.title, song.artist, song.comments, song.user)
+                const formattedSong = newSong.formatSongShow()
+                $('.trackBanner').append(formattedSong)
             })
           history.pushState(null, null, `/songs/${id}`)
          })
@@ -44,15 +44,22 @@ $(function() {
           Song.prototype.formatSongsIndex = function() {
             var songHtml = ''
             songHtml += `<a href="#" class="song-title" data-id=${this.id}><h4>${this.title}</h4></a> by: `
-            songHtml += `<a href="#" class="song-artist-name" data-id=${this.id}><h7>${this.artist.artist_name}</h7> </a><br>`
+            songHtml += `<h7>${this.artist.artist_name}</h7> <br>`
             songHtml += `number of comments: ${this.comments.length}`
             return songHtml
           }
 
           Song.prototype.formatSongShow = function() {
+            const comments = this.comments.map(comments => `<li>${comments.user.username} said: ${comments.content} </li>`)
+
             var songHtml = `
                             <h4>${this.title}</h4>
                             <h6>${this.artist.artist_name}</h6>
+                            <h6># of comments: ${this.comments.length}</h6>
+                            <h6>Comments:</h6>
+                            <ol>
+                            ${comments}
+                            </ol>
                            `
             return songHtml
 
--- app/assets/javascripts/users/users.js
+++ app/assets/javascripts/users/users.js
@@ -1,15 +1,4 @@
 $(function(){
-  // DJ Dashboard - Song show with next prompt on click
-  // 
-  // $("#load_songs").on("click", function(e){
-  //
-  // //   $.ajax({
-  // //     url: this.url,
-  // //     dataType: 'script'
-  // // })
-  //   e.preventDefault();
-  // });
-  //
 
 
   });
